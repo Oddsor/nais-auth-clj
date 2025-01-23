679c415c6f39e88c92dc236d42e84a1553c1ca9b
Odd Andreas Sørsæther <oddsor@users.noreply.github.com>
2025-01-23 15:59:44 +0100

    auth-middleware

--- src/nais_auth/api.clj
+++ src/nais_auth/api.clj
@@ -1,9 +1,14 @@
 (ns nais-auth.api
   (:import
-    [org.jose4j.jwk HttpsJwks]
-    [org.jose4j.jwt JwtClaims]
-    [org.jose4j.jwt.consumer InvalidJwtException JwtConsumer JwtConsumerBuilder]
-    [org.jose4j.keys.resolvers HttpsJwksVerificationKeyResolver]))
+   [java.net URI URLEncoder]
+   [java.nio.charset StandardCharsets]
+   [java.net.http HttpClient HttpRequest HttpRequest$BodyPublishers HttpResponse HttpResponse$BodyHandlers]
+   [org.jose4j.jwk HttpsJwks]
+   [org.jose4j.jwt JwtClaims]
+   [org.jose4j.jwt.consumer InvalidJwtException JwtConsumer JwtConsumerBuilder]
+   [org.jose4j.keys.resolvers HttpsJwksVerificationKeyResolver]))
+
+(def http-client (HttpClient/newHttpClient))
 
 (defn get-consumer ^JwtConsumer [jwk-url expected-audiences]
   (assert (string? jwk-url) "jwk-url should be a string")
@@ -19,4 +24,44 @@
     (catch InvalidJwtException _
       nil)))
 
-(comment (verify-token nil nil))
+(defn make-form-data [data]
+  (String/join "&" (map (fn [[k v]]
+                          (str (URLEncoder/encode (cond-> k (keyword? k) name) StandardCharsets/UTF_8)
+                               "="
+                               (URLEncoder/encode v StandardCharsets/UTF_8))) data)))
+
+(defn get-obo-token
+  ([token scope]
+   (let [env-vars {:token-url (System/getenv "AZURE_OPENID_CONFIG_TOKEN_ENDPOINT")
+                   :client-id (System/getenv "AZURE_APP_CLIENT_ID")
+                   :client-secret (System/getenv "AZURE_APP_CLIENT_SECRET")}]
+     (get-obo-token env-vars token scope)))
+  ([{:keys [token-url client-id client-secret] :as _config} token scope]
+   (let [request (-> (HttpRequest/newBuilder)
+                     (.uri (URI/create token-url))
+                     (.header "Content-Type" "application/x-www-form-urlencoded")
+                     (.POST (HttpRequest$BodyPublishers/ofString
+                             (make-form-data
+                              {:grant_type "urn:ietf:params:oauth:grant-type:jwt-bearer"
+                               :client_id client-id
+                               :client_secret client-secret
+                               :assertion token
+                               :scope scope
+                               :requested_token_use "on_behalf_of"})
+                             StandardCharsets/UTF_8))
+                     (.build))
+         result (HttpClient/.send http-client request (HttpResponse$BodyHandlers/ofString))]
+     (when (= (HttpResponse/.statusCode result) 200)
+       (let [body (HttpResponse/.body result)]
+         (-> (re-seq #"\"access_token\".*?:.*?\"(.*?)\"" body)
+             first
+             second))))))
+
+(defn wrap-authentication [handler & {:keys [jwk-url expected-audiences]}]
+  (let [ensured-jwk-url (if jwk-url jwk-url (System/getenv "AZURE_OPENID_CONFIG_JWKS_URI"))
+        consumer (get-consumer ensured-jwk-url expected-audiences)]
+    (fn [req]
+      (handler (if-let [auth (get-in req [:headers "authorization"])]
+                 (let [verified-token (verify-token consumer (String/.replace auth "Bearer " ""))]
+                   (assoc req :authorization verified-token))
+                 req)))))
--- test/nais_auth/api_test.clj
+++ test/nais_auth/api_test.clj
@@ -1,12 +1,12 @@
 (ns nais-auth.api-test
   (:require
-    [babashka.http-client :as http]
-    [charred.api :as charred]
-    [clojure.test :refer [deftest is testing use-fixtures]]
-    [nais-auth.api :as sut])
+   [babashka.http-client :as http]
+   [charred.api :as charred]
+   [clojure.test :refer [deftest is testing use-fixtures]]
+   [nais-auth.api :as sut])
   (:import
-    [no.nav.security.mock.oauth2 MockOAuth2Server OAuth2Config]
-    [no.nav.security.mock.oauth2.token DefaultOAuth2TokenCallback]))
+   [no.nav.security.mock.oauth2 MockOAuth2Server OAuth2Config]
+   [no.nav.security.mock.oauth2.token DefaultOAuth2TokenCallback]))
 
 (def ^:dynamic *server* nil)
 
@@ -37,4 +37,75 @@
                                    (get-token issuer))))
 
       (is (nil? (sut/verify-token (sut/get-consumer (:jwks_uri well-known-data) ["default"])
-                                   (get-token "bad")))))))
+                                  (get-token "bad")))))))
+
+(deftest can-produce-on-behalf-of-token
+  (testing "Test if a on-behalf-of-token provided by a mock-server can be validated using its public jwk"
+    (let [issuer "default"
+          well-known-url (str (MockOAuth2Server/.wellKnownUrl *server* issuer))
+          well-known-data (-> (http/request {:method :get
+                                             :uri    well-known-url
+                                             :as     :stream})
+                              :body
+                              (charred/read-json :key-fn keyword))
+          token (get-token issuer)
+          token-url (:token_endpoint well-known-data)]
+      #_(http/request {:uri token-url
+                       :method :post
+                       :form-params {:grant_type "urn:ietf:params:oauth:grant-type:jwt-bearer"
+                                     :client_id "1234567"
+                                     :client_secret "1234567"
+                                     :assertion token
+                                     :scope "dev-gcp.arbeidsgiver.tiltaksgjennomforing"
+                                     :requested_token_use "on_behalf_of"}})
+      (is (some? (sut/verify-token (sut/get-consumer (:jwks_uri well-known-data) ["dev-gcp.arbeidsgiver.tiltaksgjennomforing"])
+                                   (sut/get-obo-token {:token-url token-url
+                                                       :client-id "123456"
+                                                       :client-secret "123456"} token "dev-gcp.arbeidsgiver.tiltaksgjennomforing"))))
+
+      (is (nil? (sut/verify-token (sut/get-consumer (:jwks_uri well-known-data) ["dev-gcp.arbeidsgiver.tiltaksgjennomforing"])
+                                  (sut/get-obo-token {:token-url token-url
+                                                      :client-id "123456"
+                                                      :client-secret "123456"} token "bla")))))))
+
+(deftest wrapper-can-auth-request
+  (testing "Test if the ring middleware function can handle requests with an auth header"
+    (let [issuer "default"
+          well-known-url (str (MockOAuth2Server/.wellKnownUrl *server* issuer))
+          well-known-data (-> (http/request {:method :get
+                                             :uri    well-known-url
+                                             :as     :stream})
+                              :body
+                              (charred/read-json :key-fn keyword))
+          token (get-token issuer)
+          auth-holder (volatile! nil)
+          handler (sut/wrap-authentication (fn [req]
+                                             (vreset! auth-holder (-> req :authorization))
+                                             {:status 200
+                                              :body "Ok!"})
+                                           {:jwk-url (:jwks_uri well-known-data)
+                                            :expected-audiences ["default"]})]
+      #_(http/request {:uri token-url
+                       :method :post
+                       :form-params {:grant_type "urn:ietf:params:oauth:grant-type:jwt-bearer"
+                                     :client_id "1234567"
+                                     :client_secret "1234567"
+                                     :assertion token
+                                     :scope "dev-gcp.arbeidsgiver.tiltaksgjennomforing"
+                                     :requested_token_use "on_behalf_of"}})
+      (handler {:uri "/"
+                :method :get
+                :headers {"authorization" (str "Bearer " token)}})
+      (is (some? @auth-holder))
+      (vreset! auth-holder nil)
+
+      (handler {:uri "/"
+                :method :get
+                :headers {}})
+      (is (nil? @auth-holder)))))
+
+(comment
+  (with-server
+    can-produce-on-behalf-of-token)
+  (with-server
+    wrapper-can-auth-request))
